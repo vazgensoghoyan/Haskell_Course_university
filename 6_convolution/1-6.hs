62a47934cbc91d4e632448a21c11571c3d5e1398
vazgensoghoyan <sogvazulchik@gmail.com>
2025-10-24 12:15:03 +0300

    task 4 in hw 6

--- 6_convolution/1-6.hs
+++ 6_convolution/1-6.hs
@@ -27,13 +27,38 @@ ini2' = [[]]
 
 -- task 3
 reverse' :: [a] -> [a]
-reverse' = foldr fun' ini'
-fun' :: a -> [a] -> [a]
-fun' a arr = arr ++ [a]
-ini' = []
+reverse' = foldr fun3' ini3'
+fun3' :: a -> [a] -> [a]
+fun3' a arr = arr ++ [a]
+ini3' = []
 
 reverse'' :: [a] -> [a]
-reverse'' = foldl fun'' ini''
-fun'' :: [a] -> a -> [a]
-fun'' arr a = a:arr
-ini'' = []
+reverse'' = foldl fun3'' ini3''
+fun3'' :: [a] -> a -> [a]
+fun3'' arr a = a:arr
+ini3'' = []
+
+-- task 4
+infixl 9 !!!
+
+(!!!) :: [a] -> Int -> Maybe a
+xs !!! n = foldr fun4 ini4 xs n 
+
+fun4 :: a -> (Int -> Maybe a) -> Int -> Maybe a
+fun4 head tail n
+    | n < 0 = Nothing
+    | n == 0 = Just head
+    | otherwise = tail (n-1)
+
+ini4 :: Int -> Maybe a
+ini4 = const Nothing
+
+-- task 5
+foldl'' :: (b -> a -> b) -> b -> [a] -> b
+foldl'' f v xs = foldr (fun f) ini xs v
+
+fun :: (b -> a -> b) -> a -> (b -> t4) -> b -> t4
+fun f head tail v = tail (v `f` head)
+
+ini :: a -> a
+ini = id

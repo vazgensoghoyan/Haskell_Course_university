4fb988879caf9308d6697a23ce08e2b43437f34a
vazgensoghoyan <sogvazulchik@gmail.com>
2025-10-30 15:45:45 +0300

    task 2 and messages from chat in practice (there are solutions)

--- 7_applicative_functors/1-8.hs
+++ 7_applicative_functors/1-8.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 
 -- task 1
+
 data E l r = L l | R r
     deriving (Eq, Show)
 
@@ -21,3 +23,10 @@ instance Applicative (E l) where
   
 
 -- task 2
+
+newtype Cmps f g x = Cmps { getCmps :: f (g x) }
+
+instance (Functor f, Functor g) => Functor (Cmps f g) where
+  fmap :: (a -> b) -> Cmps f g a -> Cmps f g b
+  fmap func (Cmps cmps) = Cmps ( fmap ( fmap func ) cmps )
+  

6b9f618bcad513e6fe5a1238284785f9a45ce71d
vazgensoghoyan <sogvazulchik@gmail.com>
2025-12-04 00:08:28 +0300

    started hw 11, done task 1

---
+++ 11_monad_transformers/1-5.hs
@@ -0,0 +1,31 @@
+import Control.Monad.Identity
+
+-- task 1
+
+newtype StrRdrT m a = StrRdrT { runStrRdrT :: String -> m a }
+
+instance Functor m => Functor (StrRdrT m) where
+    fmap :: Functor m => (a -> b) -> StrRdrT m a -> StrRdrT m b
+    fmap f rd = StrRdrT $ \s -> fmap f (runStrRdrT rd s)
+
+instance Applicative m => Applicative (StrRdrT m) where
+    pure :: a -> StrRdrT m a
+    pure = StrRdrT . const . pure
+
+    (<*>) :: Applicative m => StrRdrT m (a -> b) -> StrRdrT m a -> StrRdrT m b
+    fr <*> r = StrRdrT $ \s -> runStrRdrT fr s <*> runStrRdrT r s
+
+instance Monad m => Monad (StrRdrT m) where
+    return :: a -> StrRdrT m a
+    return = pure
+
+    (>>=) :: StrRdrT m a -> (a -> StrRdrT m b) -> StrRdrT m b
+    rd >>= k = StrRdrT $ \s -> do
+        a <- runStrRdrT rd s
+        runStrRdrT (k a) s
+
+instance MonadFail m => MonadFail (StrRdrT m)  where
+    fail :: String -> StrRdrT m a
+    fail = StrRdrT . const . fail
+
+-- task 2

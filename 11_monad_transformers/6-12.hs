1cef0f27dc32454b5046e0d992317d1e49a695fc
vazgensoghoyan <sogvazulchik@gmail.com>
2025-12-04 01:10:45 +0300

    tasks 11-12 in hw 11

--- 11_monad_transformers/6-12.hs
+++ 11_monad_transformers/6-12.hs
@@ -5,6 +5,7 @@ import Control.Monad.Identity
 import Control.Applicative (liftA2)
 import Control.Monad.RWS
 import Control.Monad.State
+import Control.Monad.Reader
 
 -- task 1
 
@@ -63,10 +64,51 @@ instance MonadTrans LoggT where
 instance MonadState s m => MonadState s (LoggT m) where
     get :: MonadState s m => LoggT m s
     get = lift get
+
     put :: MonadState s m => s -> LoggT m ()
     put = lift . put
+
     state :: MonadState s m => (s -> (a, s)) -> LoggT m a
     state = lift . state
 
 -- task 5
 
+instance MonadReader r m => MonadReader r (LoggT m) where
+    ask :: MonadReader r m => LoggT m r
+    ask = lift ask
+
+    local :: MonadReader r m => (r -> r) -> LoggT m a -> LoggT m a
+    local f lg = LoggT $ local f (runLoggT lg)
+
+    reader :: MonadReader r m => (r -> a) -> LoggT m a
+    reader = lift . reader
+
+mapLoggT :: (m (Logged a) -> n (Logged b)) -> LoggT m a -> LoggT n b
+mapLoggT f lg = LoggT $ f (runLoggT lg)
+
+-- task 6
+
+class Monad m => MonadLogg m where
+    w2log :: String -> m ()
+    logg :: Logged a -> m a
+
+instance Monad m => MonadLogg (LoggT m) where
+    w2log :: Monad m => String -> LoggT m ()
+    w2log s = LoggT . return $ Logged s ()
+
+    logg :: Monad m => Logged a -> LoggT m a
+    logg  = LoggT . return
+
+instance MonadLogg m => MonadLogg (StateT s m) where
+    w2log :: MonadLogg m => String -> StateT s m ()
+    w2log = lift . w2log
+
+    logg :: MonadLogg m => Logged a -> StateT s m a
+    logg  = lift . logg
+
+instance MonadLogg m => MonadLogg (ReaderT r m) where
+    w2log :: MonadLogg m => String -> ReaderT r m ()
+    w2log = lift . w2log
+
+    logg :: MonadLogg m => Logged a -> ReaderT r m a
+    logg  = lift . logg
